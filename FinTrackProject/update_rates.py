92d12c44867f99dcb29e50fc7a28f3d876b804fa
Dcode22 <68872584+Dcode22@users.noreply.github.com>
2020-11-08 14:54:34 +0200

    14:54

--- FinTrackProject/update_rates.py
+++ FinTrackProject/update_rates.py
@@ -1,4 +1,8 @@
+import os
+import django
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'FinTrack.settings')
+django.setup()
 from djmoney.contrib.exchange.backends import OpenExchangeRatesBackend
 
-backend = OpenExchangeRatesBackend
+backend = OpenExchangeRatesBackend()
 backend.update_rates()
